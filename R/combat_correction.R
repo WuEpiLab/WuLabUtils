f1d5321d4c1e5693aaf1037b214e949a31595813
Frank <32803623+Arrhenia@users.noreply.github.com>
2024-03-11 10:54:14 +0800

    Updated combat_correction.R

--- R/combat_correction.R
+++ R/combat_correction.R
@@ -4,33 +4,34 @@
 #'
 #' @aliases combat_correction
 #'
+#' @import dplyr
 #' @importFrom sva ComBat
 #' @export
 
 combat_correction = function(a, b) {
   a = as.data.frame(a)
   b = as.data.frame(b)
-  
+
   # filters so that a and b contains the same rownames
   a = a %>% filter(row.names(a) %in% rownames(b))
   b = b %>% filter(row.names(b) %in% rownames(a))
-  
+
   a = as.data.frame(t(a))
   b = as.data.frame(t(b))
-  
+
   data = as.data.frame(t(rbind(a, b)))
-  
+
   a$label = 'a'
   b$label = 'b'
-  
+
   batch = rbind(a, b)
-  data_combat = as.data.frame(ComBat(dat=data, batch=batch$label))
-  
+  data_combat = as.data.frame(ComBat(dat=data, batch=batch$label, par.prior=F))
+
   print(nrow(a))
   print(ncol(data_combat))
-  
+
   new_a = data_combat[,c(1:nrow(a))]
   new_b = data_combat[,c((nrow(a)+1):ncol(data_combat))]
-  
+
   return(list(new_a, new_b))
 }
